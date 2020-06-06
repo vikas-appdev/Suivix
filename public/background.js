3e3e7423763a45a1fd5004b0bb2a5fe6aba16fcb
Mw3y <you@example.com>
2020-06-06 11:58:35 +0200

    Cleaning public

--- public/background.js
+++
@@ -1,3 +0,0 @@
-particlesJS.load('particles-js', '/ressources/particlesjs-config.json', function() {
-    console.log('callback - particles.js config loaded');
-  });
--- public/utils.js
+++
@@ -1,6 +0,0 @@
-function getUrl(url, window) {
-  var getUrl = window.location;
-  var baseUrl = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[0];
-  console.log(baseUrl + url);
-  return baseUrl + url;
-}
