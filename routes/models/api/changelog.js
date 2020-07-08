eff1a9fe87b114025f50f9bbef296c7c0e5c6851
Max <maxence1604.minecraft@gmail.com>
2020-07-08 11:12:04 +0200

    Fix

--- routes/models/api/changelog.js
+++ routes/models/api/changelog.js
@@ -8,7 +8,7 @@ var package = require('../../../package.json');
 module.exports = (req, res) => {
     res.send(`{
         "version": "${package.version}",
-        "en": "<strong>• You can now take attendance by directly going at https://suivix.xyz/attendance. (Discord command no longer needed)</strong><br><br>If you encounter a bug, or you think something is broken, please contact me on Discord:<br><br>MΛX#2231",
-        "fr": "<strong>• Vous pouvez désormais faire l'appel en allant directement sur https://suivix.xyz/attendance. (Vous n'avez plus besoin de taper \"!suivix\" sur discord.)</strong><br>- Vous pouvez désormais sélectionner plusieurs roles et salons lors de votre appel.<br><br>Si vous rencontrez un bug, ou pensez que quelque chose ne marche pas bien, merci de me contacter sur Discord :<br><br>MΛX#2231"
+        "en": "<strong>• You can now take attendance by directly going at <a href='https://suivix.xyz/attendance'>https://suivix.xyz/attendance</a>. (Discord command no longer needed)</strong><br><br>If you encounter a bug, or you think something is broken, please contact me on Discord:<br><br>MΛX#2231",
+        "fr": "<strong>• Vous pouvez désormais faire l'appel en allant directement sur <a href='https://suivix.xyz/attendance'>https://suivix.xyz/attendance</a>. (Vous n'avez plus besoin d'utiliser la commande sur discord.)</strong><br>- Vous pouvez désormais sélectionner plusieurs roles et salons lors de votre appel.<br><br>Si vous rencontrez un bug, ou pensez que quelque chose ne marche pas bien, merci de me contacter sur Discord :<br><br>MΛX#2231"
     }`);
 };
