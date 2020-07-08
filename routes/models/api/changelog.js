e917603ad319e73988b4ea61c3411deb96cacb8e
Max <maxence1604.minecraft@gmail.com>
2020-07-08 11:08:23 +0200

    New version

--- routes/models/api/changelog.js
+++ routes/models/api/changelog.js
@@ -8,7 +8,7 @@ var package = require('../../../package.json');
 module.exports = (req, res) => {
     res.send(`{
         "version": "${package.version}",
-        "en": "<strong>• Added the support for multi-roles and multi-channels attendance.</strong><br><br>If you encounter a bug, or you think something is broken, please contact me on Discord:<br><br>MΛX#2231",
-        "fr": "<strong>• Ajout d'une fonctionnalité de multi-suivi</strong><br>- Vous pouvez désormais sélectionner plusieurs roles et salons lors de votre appel.<br><br>Si vous rencontrez un bug, ou pensez que quelque chose ne marche pas bien, merci de me contacter sur Discord :<br><br>MΛX#2231"
+        "en": "<strong>• You can now take attendance by directly going at https://suivix.xyz/attendance. (Discord command no longer needed)</strong><br><br>If you encounter a bug, or you think something is broken, please contact me on Discord:<br><br>MΛX#2231",
+        "fr": "<strong>• Vous pouvez désormais faire l'appel en allant directement sur https://suivix.xyz/attendance. (Vous n'avez plus besoin de taper \"!suivix\" sur discord.)</strong><br>- Vous pouvez désormais sélectionner plusieurs roles et salons lors de votre appel.<br><br>Si vous rencontrez un bug, ou pensez que quelque chose ne marche pas bien, merci de me contacter sur Discord :<br><br>MΛX#2231"
     }`);
 };
